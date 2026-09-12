92fdbd271438c78773825b23bd63df3aeb205907
M2129 <pdieye890@gmail.com>
2026-09-12 14:16:58 +0000

    fix: deploy PHP-FPM avec Nginx sur Render

---
+++ docker/render-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+
+: "${PORT:=10000}"
+
+envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+php-fpm -D
+exec nginx -g 'daemon off;'
